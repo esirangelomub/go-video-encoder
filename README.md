68bab1ad38b4e3be8c915d3a9921f0784fe13f48
Eduardo Sirangelo <eduardo.sirangelo@gmail.com>
2022-10-28 10:30:24 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+{
+"resource_id":"client-id-01",
+"file_path":"sample.mp4"
+}
---
+++ application/services/upload_manager_test.go
@@ -0,0 +1,45 @@
+package services_test
+
+import (
+	"encoder/application/services"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/require"
+	"log"
+	"os"
+	"testing"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func TestVideoServiceUpload(t *testing.T) {
+
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("es-encoder")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+
+	err = videoService.Encode()
+	require.Nil(t, err)
+
+	videoUpload := services.NewVideoUpload()
+	videoUpload.OutputBucket = "es-encoder"
+	videoUpload.VideoPath = os.Getenv("localStoragePath") + "/" + video.ID
+
+	doneUpload := make(chan string)
+	go videoUpload.ProcessUpload(50, doneUpload)
+
+	result := <-doneUpload
+	require.Equal(t, result, "upload completed")
+}
